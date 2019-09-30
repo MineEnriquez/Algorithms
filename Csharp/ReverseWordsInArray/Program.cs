8e6f88e134d7646cb76d694107d4cce283a9bcb9
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2019-09-30 15:22:13 +0000

    Reverse words in array

---
+++ Csharp/ReverseWordsInArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ReverseWordsInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           string[] myarray = new string[5]{"Hola", "Miss", "Stphanie", "Monique", "bonita"};
+           foreach (string x in myarray){
+               char[] word = x.ToCharArray();
+               for (int i=0; i < word.Length; i++ ){
+
+               }
+           }
+        }
+    }
+}
