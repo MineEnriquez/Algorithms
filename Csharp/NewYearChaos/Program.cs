0320fc1ae2e2fc14cd86881ca94c0f18f1f14a01
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2019-12-08 14:11:42 +1142

    New year chaos code practice

---
+++ Csharp/NewYearChaos/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace NewYearChaos
+{
+
+class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int moves = 0;
+        for(int i = q.Length-1; i>=0 ; i-- )
+        {
+            //if ofset is way too big
+            if (q[i] - (i + 1) > 2) {
+                Console.WriteLine("Too chaotic");
+                return;
+            }
+            
+            for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+            if (q[j] > q[i]) moves++;
+            
+        }
+
+        Console.WriteLine(moves);
+        return;
+    }
+
+    static void Main(string[] args) {
+        int[] q = {2, 1, 5, 3, 4};
+        minimumBribes(q);
+        
+        int[] w = {2, 5, 1, 3, 4};
+        minimumBribes(w);
+        // int t = Convert.ToInt32(Console.ReadLine());
+
+        // for (int tItr = 0; tItr < t; tItr++) {
+        //     int n = Convert.ToInt32(Console.ReadLine());
+
+        //     int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))            ;
+        //     minimumBribes(q);
+        // }
+    }
+
+    static void print(Object x){
+        Console.WriteLine(x);
+    }
+}
+}
