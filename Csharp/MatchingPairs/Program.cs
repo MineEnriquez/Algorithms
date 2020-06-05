0443e4a2cae0c6396de637f8ef667d655daa2971
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-05 00:07:30 -0700

    Matching Pairs
    Matching Pairs - draft

---
+++ Csharp/MatchingPairs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// We don’t provide test cases in this language yet, but have outlined the signature for you. Please write your code below, and don’t forget to test edge cases!
+class MatchingPairs {
+  static void Main(string[] args) {
+
+    Console.WriteLine( matchingPairs("test", "ttes")); 
+  }
+  
+  private static int matchingPairs(string s, string t) {
+    int counter = 0;
+    for( int i = 0; i<s.Length; i++)
+    {
+        if (s[i] == t[i]){
+            counter ++;
+            continue;
+        }
+        else if (s[i] != t[i]){
+            int crossMatch = 0;
+            int oneSideMatch = 0;
+            for(int j = i; j<t.Length; j++){
+                if (s[i] == t[j] && s[j]==t[i]){  
+                     crossMatch = j;
+                     break; // we have found a both sided matching pair, so swapping them up will ensure that we have 2 matching pairs.
+                }
+                else if (s[i] == t[j] && s[j]!=t[i]){  
+                    oneSideMatch = j;
+                }
+            }
+            //swap 
+            
+            // TODO: FINISH.
+
+        }
+    }
+    
+    
+    return counter;
+  }
+}
