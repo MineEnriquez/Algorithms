8d1f703250bbcd583fc2975d4812e34d8f24c74c
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-06 23:30:20 -0700

    Update Program.cs
    Matching Pairs

--- Csharp/MatchingPairs/Program.cs
+++ Csharp/MatchingPairs/Program.cs
@@ -1,42 +1,56 @@
 ﻿using System;
-
-// We don’t provide test cases in this language yet, but have outlined the signature for you. Please write your code below, and don’t forget to test edge cases!
-class MatchingPairs {
-  static void Main(string[] args) {
-
-    Console.WriteLine( matchingPairs("test", "ttes")); 
-  }
-  
-  private static int matchingPairs(string s, string t) {
-    int counter = 0;
-    int oneSideMatch = 0;
-    int crossMatch = 0;
-    for( int i = 0; i<s.Length; i++)
+class MatchingPairs
+{
+    static void Main(string[] args)
     {
-        if (s[i] == t[i]){
-            counter ++;
-            continue;
-        }
-        else if (s[i] != t[i]){      
-            for(int j = i; j<t.Length; j++){
-                if (s[i] == t[j] && s[j]==t[i]){  
-                     crossMatch = j;
-                     break; // we have found a both sided matching pair, so swapping them up will ensure that we have 2 matching pairs.
-                }
-                else if (s[i] == t[j] && s[j]!=t[i]){  
-                    oneSideMatch = j;
-                }
-            }
-            //swap 
-            if ( crossMatch!= 0 ) 
-            { 
-              s[i] = s[crossMatch];
-              counter++;
-            }
-        }
+        Console.WriteLine(matchingPairs("mno", "mno"));
+        Console.WriteLine(matchingPairs("abcd", "adcb"));
+        Console.WriteLine(matchingPairs("mn", "mn"));
     }
-  
-    
-    return counter;
-  }
+
+    private static int matchingPairs(string s, string t)
+    {
+        int counter = 0;
+        bool oneSideMatch = false;
+        bool crossMatch = false;
+        bool swapped = false;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == t[i])
+            {
+                counter++;
+                continue;
+            }
+            else if (s[i] != t[i] && !swapped)
+            {
+                for (int j = i; j < t.Length; j++)
+                {
+                    if (s[i] == t[j] && s[j] == t[i])
+                    {
+                        crossMatch = true;
+                        swapped = true;
+                        break;
+                    }
+                    if (s[i] == t[j] && s[j] != t[i])
+                    {
+                        swapped = true;
+                        oneSideMatch = true;
+                    }
+                }
+            }
+        }
+        if (crossMatch || oneSideMatch)  // If we ever spotted the values to be swapped:
+        {
+            if (crossMatch)
+                counter = counter + 2;
+            else if (oneSideMatch)
+                counter++;
+        }
+        else  if ( ! swapped && counter >= 2)  // If we never found a value to be swapped, it means that all of them were the same, 
+                // therefore swapping 2 of them will reduce the counter by 2.
+            counter = counter - 2;
+
+        return counter;
+    }
 }
