7abb3588adda9385676c5949c0728bf90c05a695
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-08 23:11:21 -0700

    Encripted Words for Odd only strings
    First Part, odd only strings and substrings less than 7.

---
+++ Csharp/EncriptedWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// We don’t provide test cases in this language yet, but have outlined the signature for you. Please write your code below, and don’t forget to
+public class EncryptedWords
+{
+    static void Main(String[] args)
+    {
+        // Call findEncryptedWord with test cases here
+        Console.WriteLine(findEncryptedWord("abcxcba"));
+        Console.WriteLine("xbacbca");
+    }
+
+    private static String findEncryptedWord(String s)
+    {
+        int len = s.Length;
+        int half = 0;
+        string mid = "";
+
+        half = len / 2;
+        if (len <= 1) return s;
+        else mid = s.Substring(half, 1);
+        string a = findEncryptedWord(s.Substring(0, half));
+        string b = findEncryptedWord(s.Substring(half + 1, half));
+        return mid + a + b;
+    }
+}
