f0d06aa7ec7cd242f0031d731f5f1f14df276a63
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-08 20:55:31 -0700

    Nodes In a Sub tree
    Draft

---
+++ Csharp/NodesInASubtree/Program.cs
@@ -0,0 +1,30 @@
+﻿// We don’t provide test cases in this language yet. Please write your code below, and don’t forget to test edge cases!using 
+
+using System;
+using System.Collections;
+
+class Program{
+  static void Main(){
+     Console.WriteLine("Test")  ;
+     BinaryTree bt = new BinaryTree(12);
+     
+  }
+}
+
+class Node
+{ public Node Left { get; set;}
+  public Node Right  { get; set;}
+  public int Data  { get; set;}
+  public Node (int data) {
+   this.Data = data;  
+   Console.WriteLine(this.Data);
+  }
+}
+
+class BinaryTree
+{
+  public Node Root {get; set;}
+  public BinaryTree(int _rootNode){
+    this.Root = new Node(_rootNode);    
+  }
+}
