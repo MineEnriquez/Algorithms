89602368343e5601149113b92809613f74435bf7
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-09 01:03:13 -0700

    Update Program.cs
    Wrapping up encripted words program.

--- Csharp/EncriptedWords/Program.cs
+++ Csharp/EncriptedWords/Program.cs
@@ -1,26 +1,46 @@
 ﻿using System;
+using System.Diagnostics;
 
 // We don’t provide test cases in this language yet, but have outlined the signature for you. Please write your code below, and don’t forget to
 public class EncryptedWords
 {
     static void Main(String[] args)
     {
-        // Call findEncryptedWord with test cases here
-        Console.WriteLine(findEncryptedWord("abcxcba"));
-        Console.WriteLine("xbacbca");
+        Test("abc", "bac");
+        Test("abcxcba", "xbacbca");
+        Test("facebook", "eafcobok");
+        Test("abcd", "bacd");
     }
-
+    public static void Test(string test, string expected)
+    {
+        string actual = findEncryptedWord(test);
+        Console.WriteLine($"TEST: {test} EXPECTED: {expected}");
+        if (actual == expected) Console.WriteLine("\tPASS ");
+        else Console.WriteLine($"\tFAILED - Expected:  {expected}, Actual: { actual}");
+    }
     private static String findEncryptedWord(String s)
     {
         int len = s.Length;
-        int half = 0;
+        int half = len / 2;
         string mid = "";
+        string a = "";
+        string b = "";
 
-        half = len / 2;
-        if (len <= 1) return s;
-        else mid = s.Substring(half, 1);
-        string a = findEncryptedWord(s.Substring(0, half));
-        string b = findEncryptedWord(s.Substring(half + 1, half));
-        return mid + a + b;
+        if (len % 2 > 0)
+        {
+            if (len <= 1) return s;
+            else mid = s.Substring(half, 1);
+            a = s.Substring(0, half);
+            b = s.Substring(half + 1, len - half - 1);
+        }
+        else
+        {
+            if (len == 2) return s;
+            else mid = s.Substring(half - 1, 1);
+            a = s.Substring(0, half - 1);
+            b = s.Substring(half, len - half);
+        }
+
+        return mid + findEncryptedWord(a) + findEncryptedWord(b);
     }
 }
