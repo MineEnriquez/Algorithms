917f9c563ce7de940a748e8825b48ebaa81434da
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-06-24 00:10:16 +0530

    Javascript objects 02

---
+++ JavaScript/JsObjects_users.js
@@ -0,0 +1,15 @@
+
+
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
